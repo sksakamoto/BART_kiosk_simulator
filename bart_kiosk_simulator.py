70565f37b23eb9661318778fd7bd6241b22c8bc0
Stephanie <wondafury@gmail.com>
2016-04-30 21:32:04 -0700

    BART kiosk simulator part 2

--- bart_kiosk_simulator.py
+++ bart_kiosk_simulator.py
@@ -5,8 +5,26 @@ def load_card(num1, num5, num10, num20):
 	twenties = num20*20
 	total = ones + fives + tens + twenties
 	return total
-print load_card(0,0,0,0)
-print load_card(0,0,0,9)
-print load_card(2,3,0,0)
-print load_card(3,1,1,3)
+
+clipper_card1 = load_card(1,0,0,0)
+clipper_card2 = load_card(0,0,0,1)
+clipper_card3 = load_card(1,1,0,0)
+clipper_card4 = load_card(2,1,1,0)
+
+DUBLIN_TO_POWELL = 6.15
+FRUITVALE_TO_UNION_CITY = 3.80
+ORINDA_TO_RICHMOND = 3.35
+HAYWARD_TO_CONCORD = 5.20
+FREMONT_TO_COLMA = 6.60
+
+def travel_to_destination(fare_price, card_value):
+	if card_value >= fare_price:
+		print "Welcome aboard, enjoy your trip!"
+	else: 
+		print "You need more money!"
+
+print travel_to_destination(clipper_card1, FREMONT_TO_COLMA)
+print travel_to_destination(clipper_card2, HAYWARD_TO_CONCORD)
+print travel_to_destination(clipper_card3, DUBLIN_TO_POWELL)
+print travel_to_destination(clipper_card4, FRUITVALE_TO_UNION_CITY)
 
